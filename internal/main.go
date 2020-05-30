22bc3526293805e54c1b648ad0808d69ccce9b86
Thun Pin Chiu <thunpin@gmail.com>
2020-05-30 23:59:40 +0200

    running the application on port 8080

--- internal/main.go
+++ internal/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("vim-go")
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "Hello, %q", html.EscapeString(request.URL.Path))
 	})
+
+	log.Println("Listening on port :8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
