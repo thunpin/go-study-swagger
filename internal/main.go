f5021ef77b97efc65d5dd859752156515e6595e5
Thun Pin Chiu <thunpin@gmail.com>
2020-05-31 11:36:25 +0200

    implements the api

--- internal/main.go
+++ internal/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/thunpin/go-study-swagger/pkg/swagger/server/restapi"
 	"github.com/thunpin/go-study-swagger/pkg/swagger/server/restapi/operations"
 )
@@ -17,14 +18,30 @@ func main() {
 
 	api := operations.NewHelloAPI(swaggerSpec)
 	server := restapi.NewServer(api)
-	defer server.Shutdown()
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
 	server.Port = 8080
 	log.Printf("Listening on port %d", server.Port)
 
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+
 	// Start listening using having the handlers and port
-	// already set up.
 	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+//Health route returns OK
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+//GetHelloUser returns Hello + your name
+func GetHelloUser(params operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + params.User + "!")
 }
